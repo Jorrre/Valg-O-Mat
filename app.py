1fc274aab84527b8f63bde86e834722705d69809
Fredrik Hagland <hagland.fredrik@gmail.com>
2017-10-27 08:32:08 +0200

    Update app.py
    Updated to newer version

--- app.py
+++ app.py
@@ -2,42 +2,52 @@
 
 from flask import Flask, render_template, request, redirect, url_for
 import json
+import random
 app = Flask(__name__)
 
-with open('static/data.json') as f:
-    a = json.load(f)
+with open('./static/data.json') as data:
+    j = json.load(data)
 
 # Variables
 answers = []
+rankings = [
+    "Du bør kanskje vurdere en annen studierettning.",
+    "Elektrofag med studiekompetanse ser ut til å passe for deg!",
+    "Maske og hår passer fint for deg! Snakk med Åse fra Westerdals! Hilsen Oberst Tonmäster Herland!"
+]
+
 g = 0
+varBtn = "Neste"
 
 @app.route('/')
 @app.route('/<page>')
-def main(page=None):
+def index(page=None):
     if page == None:
         return render_template('index.html')
     else:
-        return render_template('slides.html', question=a['spm'][0]['q'])
+        return render_template('slides.html', question=j['spm'][0]['q'], varBtn="Neste Spørsmål")
 
 @app.route('/next', methods=['GET', 'POST'])
 def next():
     global g
-    if request.form['sender'] == 'NEXT':
-        # Next Question
-        if g == 10:
-            render_template('done.html')
-        else:
-            g += 1
-            if request.method == 'POST':
-                return render_template('slides.html', question=a['spm'][g]['q'], counter=g)
+    global answers
+    answers.append(request.form['input'])
+    print(g)
+    print(len(request.form['input']))
 
-@app.route('/done', methods=['GET', 'POST'])
-def done():
-    '''
-        Her må vekttabellene legges sammen
-        og resultatene må indekseres.
-    '''
-    return render_template('done.html')
+    if len(request.form['input']) > 1:
+        if request.form['sender'] == 'NEXT':
+            if g > 9:
+                return render_template('done.html', rankings=rankings)
+            else:
+                g += 1
+                if request.method == 'POST':
+                    if g == 9:
+                        return render_template('slides.html', question=j['spm'][g]['q'], varBtn = "Fullfør")
+                    else:
+                        return render_template('slides.html', question=j['spm'][g]['q'], varBtn = "Neste Spørsmål")
+    else:
+        return "NOOOOO"
 
 if __name__ == "__main__":
     app.run()
