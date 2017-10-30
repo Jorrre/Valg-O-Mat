d7410df0d21362e378f3b92d477bb22bf74d49f3
jorre20 <33089213+jorre20@users.noreply.github.com>
2017-10-30 09:25:26 +0100

    Update standalone_methods.py

--- standalone_methods.py
+++ standalone_methods.py
@@ -3,7 +3,7 @@ import json
 with open('./static/data.json') as data:
     j = json.load(data)
 
-answers = ['Nei', 'Nei', 'Nei', 'Nei', 'Nei', 'Nei', 'Nei', 'Nei', 'Nei', 'Nei', 'Nei']
+answers = []
 winners_alt = []
 
 
@@ -22,7 +22,7 @@ def result(user_answers):
             for i in range(0, len(rankings) - 1):
                 rankings[i] += j['spm'][e]['vekt'][1][i]
 
-    # Kopierer rankings[] til winners[] for å sortere winners
+    # Kopierer rankings[] til winners[] for å sortere winners[]
     winners = []
     for k in rankings:
         winners.append(k)
@@ -31,11 +31,11 @@ def result(user_answers):
     # Sorterer svaralternativstringene
     for x in range(0, len(winners) - 1):
         for y in range(0, len(rankings) - 1):
-            if winners[x] == rankings[y]:
+            if winners[x] == rankings[y] and j['alt'][y] not in winners_alt:
                 winners_alt.append(j['alt'][y])
                 break
-
-    print(winners_alt)
+            elif winners[x] == rankings[y] and j['alt'][y] in winners_alt:
+                winners_alt.append(j['alt'][y + 1])
 
     # Endrer på plasseringsnummerene hvis det er flere svaralternativer som har like mange poeng
     for f in range(0, len(winners_alt) - 1):
@@ -43,13 +43,12 @@ def result(user_answers):
             # sett plasseringstallet til winners_alt[f + 1] til winners_alt[f]
             print("")
 
-result(answers)
-
 '''
     --== NOTES ==--
     winners_alt[] inneholder j['alt'] stringene i sortert rekkefølge
     Bytt navn på rankings[] til winners_alt[] i app.py og fjern alle verdiene.
     rankings[] blir definert som et lokalt array inne i result().
     rankings i done.html må byttes ut med winners_alt
+    Fungerer som den skal, bortsett fra æ, ø og å.
     --== END ==--
 '''
