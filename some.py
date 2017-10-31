d0e0a9692276a0ecc1f71c2e4bbee8ea5a082220
jorre20 <33089213+jorre20@users.noreply.github.com>
2017-10-31 12:01:30 +0100

    Update and rename standalone_methods.py to some.py

---
+++ some.py
@@ -0,0 +1,33 @@
+
+def result(user_answers, data_file):
+    winners_alt = []
+    rankings = [0, 0, 0, 0, 0, 0]
+
+    # Summerer opp poengene
+    for e in range(0, len(user_answers)):
+        if user_answers[e] == 'Ja':
+            for i in range(0, len(rankings)):
+                rankings[i] += data_file['spm'][e]['vekt'][0][i]
+        elif user_answers[e] == 'Nei':
+            for i in range(0, len(rankings)):
+                rankings[i] += data_file['spm'][e]['vekt'][2][i]
+        else:
+            for i in range(0, len(rankings)):
+                rankings[i] += data_file['spm'][e]['vekt'][1][i]
+
+    # Kopierer rankings[] til winners[] for å sortere winners[]
+    winners = []
+    for k in rankings:
+        winners.append(k)
+    winners.sort(reverse=True)
+
+    # Sorterer svaralternativstringene
+    for x in range(0, len(winners)):
+        for y in range(0, len(rankings)):
+            if winners[x] == rankings[y] and data_file['alt'][y] not in winners_alt:
+                winners_alt.append(data_file['alt'][y])
+                break
+            elif winners[x] == rankings[y] and data_file['alt'][y] in winners_alt:
+                winners_alt.append(data_file['alt'][y + 1])
+
+    return winners_alt
