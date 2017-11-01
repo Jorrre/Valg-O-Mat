9d47ce073933582125b574ad54b155fb7574a5e5
Fredrik Hagland <hagland.fredrik@gmail.com>
2017-11-01 12:17:54 +0100

    Update app.py
    jah

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, request, redirect, url_for
+from methods import result
 import json
 import random
 app = Flask(__name__)
-from some import result
 
 with open('./static/data.json') as data:
     j = json.load(data)
 
 # Variables
 answers = []
-
+rankings = []
+winners_alt = []
 g = 0
-qt = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-random.shuffle(qt)
-
-varBtn = "Neste"
 
 @app.route('/')
 @app.route('/<page>')
@@ -31,23 +28,30 @@ def next():
     global g
     global answers
     answers.append(request.form['input'])
-    print(g)
-    print(len(request.form['input']))
 
-    if len(request.form['input']) > 1:
-        if request.form['sender'] == 'NEXT':
-            if g > 9:
-                return render_template('done.html', rankings=rankings)
-            else:
-                g += 1
-                if request.method == 'POST':
-                    y = qt[g]
-                    if g == 9:
-                        return render_template('slides.html', question=j['spm'][y]['q'], varBtn = "Fullfør")
-                    else:
-                        return render_template('slides.html', question=j['spm'][y]['q'], varBtn = "Neste Spørsmål")
-    else:
-        return "NOOOOO" #eller "Du må velge et av alternativene"
+    if request.form['sender'] == 'NEXT':
+        if g > 10:
+            fuck = result(answers, j)
+            return render_template('done.html', rankings=fuck)
+        else:
+            g += 1
+            if request.method == 'POST':
+                if g == 9:
+                    return render_template('slides.html', question=j['spm'][g]['q'], varBtn = "Fullfør")
+                else:
+                    return render_template('slides.html', question=j['spm'][g]['q'], varBtn = "Neste Spørsmål")
+
+@app.route('/restart', methods=['GET', 'POST'])
+def restart():
+    # Resetting all variables
+    global g
+    global answers
+    g = 0
+    del answers[:]
+
+    # Returning user to the startpage
+    return render_template('index.html')
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
